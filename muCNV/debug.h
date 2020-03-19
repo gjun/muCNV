99680da3e5dc9aaa0e010e2c3449116353ac407b
Goo Jun <goo.jun@gmail.com>
2020-03-19 12:36:58 -0500

    debug.h

--- muCNV/debug.h
+++ muCNV/debug.h
@@ -9,20 +9,15 @@
 #ifndef debug_h
 #define debug_h
 
-#ifdef DDEBUG
-#define DDEBUG_TEST 1
-#else
-#define DDEBUG_TEST 0
-#endif
-
 #ifdef DEBUG
 #define DMSG(str) do { std::cerr << str << std::endl; } while( false )
 #else
 #define DMSG(str) do { } while ( false )
 #endif
 
-#define DDMSG(str) do { if (DDEBUG_TEST) std::cerr << str << std::endl; } while( false )
-#define DDPRINT(fmt, ...) do { if (DDEBUG_TEST) fprintf(stderr, fmt, __VA_ARGS__); } while ( false )
-
+#ifdef DDEBUG
+#define DDMSG(str) do { std::cerr << str << std::endl; } while( false )
+#define DDPRINT(fmt, ...) do { fprintf(stderr, fmt, __VA_ARGS__); } while ( false )
+#endif
 
 #endif /* debug_h */
